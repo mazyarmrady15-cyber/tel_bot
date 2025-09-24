5d4f0f5da2038deb350654ac5f53d2d1d75bc043
mazyarmrady15-cyber <mazyarmrady15@gmail.com>
2025-09-24 12:33:28 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ import speech_recognition as sr
 from moviepy.editor import VideoFileClip
 
 # ---------------- Config ----------------
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8270631879:AAEXhJ9G_5PPLUUSiqYBgnRpZZ3RNlAp0kY")
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN env var is required")
 
